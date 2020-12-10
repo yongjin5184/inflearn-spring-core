ed0279bc9a55bd6fd9765b3ff8bc933c5e10319e
Kimyongjin <yongjin5184@gmail.com>
2020-12-10 10:03:48 +0900

    인프런 스프링 핵심 원리 - Member.java, Grade.java 추가

---
+++ src/main/java/com/example/inflearnspring/core/member/Grade.java
@@ -0,0 +1,5 @@
+package com.example.inflearnspring.core.member;
+
+public enum Grade {
+    VIP, BASIC;
+}
---
+++ src/main/java/com/example/inflearnspring/core/member/Member.java
@@ -0,0 +1,26 @@
+package com.example.inflearnspring.core.member;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+}
