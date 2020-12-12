4aab3c20de73f7f496ee0d94e32bae9e1048b42e
Kimyongjin <yongjin5184@gmail.com>
2020-12-13 08:52:51 +0900

    인프런 스프링 핵심 원리 - 스프링 없는 순수한 DI 컨테이너 동일성 테스트

---
+++ src/test/java/com/example/inflearnspring/core/domain/singleton/SingletonTest.java
@@ -0,0 +1,23 @@
+package com.example.inflearnspring.core.domain.singleton;
+
+import com.example.inflearnspring.core.AppConfig;
+import com.example.inflearnspring.core.domain.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
