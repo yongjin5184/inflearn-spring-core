629e88a9a1ab28d2a6cfa5238ff5e2f5b8a3930d
Kimyongjin <yongjin5184@gmail.com>
2020-12-13 13:46:05 +0900

    인프런 스프링 핵심 원리 - Component Scan filter 에 사용할 어노테이션 추가, 어노테이션 사용한 Bean 추가

---
+++ src/main/java/com/example/inflearnspring/core/domain/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.example.inflearnspring.core.domain.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/main/java/com/example/inflearnspring/core/domain/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.example.inflearnspring.core.domain.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
