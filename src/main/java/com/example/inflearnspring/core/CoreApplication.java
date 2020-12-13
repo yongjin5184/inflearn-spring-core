44b14b2832fdb8f7b7a8e080bbdd1cf6a3b19108
Kimyongjin <yongjin5184@gmail.com>
2020-12-13 18:30:20 +0900

    인프런 스프링 핵심 원리 - @SpringBootApplication annotataion 추가

--- src/main/java/com/example/inflearnspring/core/CoreApplication.java
+++ src/main/java/com/example/inflearnspring/core/CoreApplication.java
@@ -3,9 +3,11 @@ package com.example.inflearnspring.core;
 import com.example.inflearnspring.core.domain.member.Grade;
 import com.example.inflearnspring.core.domain.member.Member;
 import com.example.inflearnspring.core.domain.member.service.MemberService;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class CoreApplication {
 
     public static void main(String[] args) {
