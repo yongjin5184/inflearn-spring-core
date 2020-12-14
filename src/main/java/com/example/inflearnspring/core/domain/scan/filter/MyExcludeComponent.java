146253c5681aeaa68ca3ee75e4116ade74e14bd5
Kimyongjin <yongjin5184@gmail.com>
2020-12-14 20:26:24 +0900

    인프런 스프링 핵심 원리 - MyExcludeComponent 추가

---
+++ src/main/java/com/example/inflearnspring/core/domain/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package com.example.inflearnspring.core.domain.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
